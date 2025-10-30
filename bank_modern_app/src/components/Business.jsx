46055793e9dc83830da7f5b6c26bf519e96818fb
Meklit <meklitshiferaw61@gmail.com>
2025-10-30 10:58:11 -0700

    modern bank

---
+++ bank_modern_app/src/components/Business.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Business = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Business
---
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardDeal = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default CardDeal
