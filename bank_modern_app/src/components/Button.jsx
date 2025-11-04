edfb310a03ee0a7dd2e82b706231cb7a16553e53
Meklit <meklitshiferaw61@gmail.com>
2025-11-04 02:57:58 -0800

    modern bank frontend

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import styles from '../style.js'
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div></div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]` }>
+      Get Started
+    </button>
   )
 }
 
